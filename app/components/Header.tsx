34269481ed3361e05596e44896b2da77f57da958
AIT ASSOU ABDELKADER <pasarela70@hotmail.com>
2025-01-02 22:49:34 -0500

    add task form as content of the modal when day cell is clicked

---
+++ app/components/Header.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import theme from "../theme.json"
+import { Button, Icon, IconElement, Text } from '@ui-kitten/components'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+interface HeaderProps {
+    username: string;
+}
+
+
+
+const Header: React.FC<HeaderProps> = ({username}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.username} category='h6'>{username}</Text>
+        <TouchableOpacity>
+        <Ionicons name="exit-outline" size={34} color="#EDB232" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    container: {
+        height: 100,
+        backgroundColor: theme["gradient-from"],
+        justifyContent:"center",
+    },
+
+    row: {
+        flexDirection:"row",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingTop: 40,
+        paddingHorizontal: 10
+    },
+
+    icon: {
+        width: 32,
+        height: 32,
+      },
+
+      username: {
+        color:"#EDB232",
+        fontWeight: 900,
+        letterSpacing: 1
+      }
+
+
+})
